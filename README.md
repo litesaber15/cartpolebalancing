6dece04fa433f8194cdd259aaceb6e34ab061699
Kartikeya <kartikeya1994@gmail.com>
2016-11-25 13:49:22 -0500

    Adjusted hyperparams, cleaned code, updated README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # cartpolebalancing
-Policy iteration algorithm for cart pole balancing problem, WIP.
+Using Policy Iteration to solve the Cart Pole Balancing problem. A simple 1 hidden layer fully connected neural network is used to evaluate the best action for a given state. Suppose a training episode lasts for `k` steps. Reward for each step is collected, and discounted return is calculated for each step after the episode ends. (state,discounted return) is stored for each each episode. Backpropogration is done for a batch of episodes, and the process is repeated for a number of batches. 
+
+Here's a GIF of the trained AI:
+![Screen GIF](/screen.gif?raw=true "GIF of trained AI")
+
+Simulation environment: [OpenAI Gym Cartpole-v0](https://gym.openai.com/envs/CartPole-v0)
+
+Forward pass and backpropogation done in Theano. Here are good tutorial for [getting started with Theano](http://nbviewer.jupyter.org/github/craffel/theano-tutorial/blob/master/Theano%20Tutorial.ipynb) and for [implementing a simple ANN](http://outlace.com/Beginner-Tutorial-Theano/).
+
+I used the CPU for this. The Nvidia drivers are a bit tricky to install on Ubuntu 1604 if you have Intel's Skylake. Here's my Theano `.theanorc` config for CPU:
+```
+[global]
+floatX = float32
+device = cpu
+force_device=True
+pycuda.init = False
+
+[lib]
+cnmem = 1
+
+[blas]
+ldflags=-L/usr/lib/ -lblas
+```
--- cartpole.py
+++ cartpole.py
@@ -8,36 +8,36 @@ from pprint import pprint
 env = gym.make('CartPole-v0')
 env.reset()
 
-training_batch_size = 8
-num_batches = 60
-e = 0.1# epsilon greedy action selection probability
-e_discount = 0.98
+training_batch_size = 10
+num_batches = 100
+e = 0.3# epsilon greedy action selection probability
+e_discount = 1
 discount = 1 #discount factor while calculating returns
 display = False # whether to render graphic
-monitor = False #whether to monitor the run and save video
+monitor = True #whether to monitor the run and save video
 #state is defined by cart position, cart velocity, pole angle, pole tip velocity
 dim_state = 4 #dimension of state vector
 # there are two possible actions: move cart left(0) or move cart right (1)
 dim_action = 2 #dimension of action vector
-wid_hidden = 10#no. of nodes in hidden layer
-L1_reg = 1
-L2_reg = 1
+wid_hidden = 5#no. of nodes in hidden layer
+L1_reg = 1 #weight of L1 regularization term
+L2_reg = 1 #weight of L2 regularization term
 
 state = T.dvector()
-action_probabilities = T.dvector() # predicted value
-target_actions = T.dvector()
-ret = T.dscalar()
-ret_vector = T.dvector()
-action_vector = T.dvector()
+action_probabilities = T.dvector() # score of each action evaluated by the NN for a state
+ret = T.dscalar() # return for an action
+ret_vector = T.dvector() # reward times action vector
+action_vector = T.dvector() # one hot vector representing which action was chosen
 
 def layer(x, w):
-	b = np.array([1], dtype=theano.config.floatX)
+	b = np.array([1], dtype=theano.config.floatX) #add bias term
 	new_x = T.concatenate([x, b])
 	m = T.dot(w.T, new_x)
 	h = nnet.sigmoid(m)
 	return h
 
 def softmax_layer(x,w):
+	# last layer is softmax layer since it represents probabilities of actions to pick, and should sum to 1
 	return T.nnet.softmax(layer(x,w)).reshape((2,))
 
 def flip(a):
@@ -60,7 +60,7 @@ def e_greedy(action_probabilities, off=False):
 		return flip(action)
 	return action
 
-# randomly initialize weights of two hidden layers
+# randomly initialize weights of hidden layer and output layer
 # these are initialized as shared variables that are updated by theano
 # +1 in input dimension is for bias
 w1 = theano.shared(np.array(np.random.rand(dim_state+1,wid_hidden), dtype=theano.config.floatX))
@@ -72,6 +72,7 @@ output_layer = softmax_layer(hidden_layer, w2)
 
 L1 = abs(w1).sum() + abs(w2).sum() #L1 norm regularization term
 L2 = (w1**2).sum() + (w2**2).sum() #L2 norm regularization term
+
 #cost equation
 mse_fc = T.sum(T.log(T.dot(output_layer,-ret_vector)))#+ L1_reg*L1 + L2_reg*L2
 updates = Adam(mse_fc, [w1,w2])
@@ -79,10 +80,10 @@ updates = Adam(mse_fc, [w1,w2])
 #compile theano functions
 backprop = theano.function(inputs=[state,ret_vector], outputs=mse_fc, updates=updates)
 run_forward = theano.function(inputs=[state], outputs=output_layer)
-if monitor:
-	env.monitor.start('/tmp/cartpole-monitor2', force=True)
+
+#train neural net
 for batch_num in range(num_batches):
-	e *= e_discount
+	e *= e_discount # randomness in action selection reduced with each batch
 	max_steps = 0
 	avg_steps = 0.0
 	batch_ret_vectors = []
@@ -96,11 +97,12 @@ for batch_num in range(num_batches):
 		ret_vectors = []
 
 		#an episode is one complete run of cart-pole 
-		#epoch = each step in an episode where we take one action
+		#a step in an episode is a point where we can take an action
 
 		#run an episode and store states,rewards of each step
 		step = 0
 		done = False
+		#episode begins
 		while not done and step <= 200:
 			states.append(state)
 			if display:
@@ -116,13 +118,12 @@ for batch_num in range(num_batches):
 			rewards.append(reward)
 			step += 1
 		batch_states.append(states)
+		#episode ends
 
-		#compute and print some metrics
 		avg_steps = (avg_steps*train_iter + step)/(train_iter+1)
 		if step>max_steps:
 			max_steps = step
-		#print('Iter: '+str(train_iter)+' Steps: '+str(step)+' Max: '+str(max_steps)+ ' Avg: '+str(avg_steps))
-		#pprint(rewards)
+
 	 	# calculate discounted return for each step
 		for i in range(len(states)):  
 			ret = 0
@@ -136,17 +137,19 @@ for batch_num in range(num_batches):
 		batch_ret_vectors.append(ret_vectors)
 
 	#backprop discounted return
-	for i in range(training_batch_size):
-		for j in range(len(batch_states[i])):
+	for i in range(training_batch_size): #for each episode in batch
+		for j in range(len(batch_states[i])): # for each step of episode
 			backprop(batch_states[i][j],batch_ret_vectors[i][j])
 
 	print('Batch: '+str(batch_num)+' Max: '+str(max_steps)+ ' Avg: '+str(avg_steps)+' Epsilon: '+str(e))
 
 # test learnt policy, e-greedy is off
 print('********\nTesting\n********')
-
+if monitor:
+	env.monitor.start('/home/kartikeya/keras/cartpole-monitor', force=True)
 max_steps = 0
 avg_steps = 0.0
+step=0
 for test_iter in range(100):		
 	state = env.reset()
 	done = False
@@ -154,7 +157,7 @@ for test_iter in range(100):
 	step = 0
 	while not done and step<200:
 		step +=1 
-		if test_iter == 199:
+		if test_iter == 99:
 			time.sleep(0.001)
 			env.render()
 		action_probabilities = run_forward(state)
